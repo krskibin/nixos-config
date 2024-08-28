83577822e6e6b88c4abc23135d894ff364b14f32
krystian <krskibin@gmail.com>
2024-08-29 00:27:17 +0200

    Zen Tmux Config

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -1,8 +1,35 @@
-{pkgs, ...}:
-
+{ pkgs, config, ... }:
 {
   programs.tmux = {
     enable = true;
+
+    shortcut = "b";
+    aggressiveResize = true;
+    baseIndex = 1;
+    newSession = true;
+    escapeTime = 0;
+    secureSocket = false;
+    keyMode = "vi";
+
+    terminal = "screen-256color";
+
     shell = "${pkgs.zsh}/bin/zsh";
+
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.better-mouse-mode
+      tmuxPlugins.resurrect
+      tmuxPlugins.catppuccin
+    ];
+
+    extraConfig = ''
+      # Fix tmux colors
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
+
+      # Change tmux theme
+      set -g @catppuccin_flavour 'mocha'
+   '';
   };
 }
