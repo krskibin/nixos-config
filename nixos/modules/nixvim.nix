842badc47776ac92421c71313aa400cadbcf6cde
krystian <krskibin@gmail.com>
2024-08-29 01:33:16 +0200

    Neovim keymaps

--- nixos/modules/nixvim.nix
+++ nixos/modules/nixvim.nix
@@ -63,6 +63,74 @@
 
       completeopt = ["menu" "menuone" "noselect"]; # For CMP plugin
     };
+
+    keymaps = [
+      # Global
+      # Default mode is "" which means normal-visual-op
+      {
+        key = "<C-n>";
+        action = "<CMD>NvimTreeToggle<CR>";
+        options.desc = "Toggle NvimTree";
+      }
+      {
+        key = "<leader>c";
+        action = "+context";
+      }
+
+      # Buffers
+      {
+        mode = "n";
+        key="<Tab>";
+        action="<CMD>bnext<CR>";
+        options.desc = "Next buffer";
+      }
+      {
+        mode = "n";
+        key="<S-Tab>";
+        action="<CMD>bprev<CR>";
+        options.desc = "Prev buffer";
+      }
+       
+      # Tabs
+      {
+        mode = "n";
+        key = "<leader>t";
+        action = "+tab";
+      }
+      {
+        mode = "n";
+        key = "<leader>tn";
+        action = "<CMD>tabnew<CR>";
+        options.desc = "Create new tab";
+      }
+      {
+        mode = "n";
+        key = "<leader>td";
+        action = "<CMD>tabclose<CR>";
+        options.desc = "Close tab";
+      }
+      {
+        mode = "n";
+        key = "<leader>ts";
+        action = "<CMD>tabnext<CR>";
+        options.desc = "Go to the sub-sequent tab";
+      }
+      {
+        mode = "n";
+        key = "<leader>tp";
+        action = "<CMD>tabprevious<CR>";
+        options.desc = "Go to the previous tab";
+      }
+
+      # Terminal
+      {
+        # Escape terminal mode using ESC
+        mode = "t";
+        key = "<esc>";
+        action = "<C-\\><C-n>";
+        options.desc = "Escape terminal mode";
+      }
+    ];
   };
 
   programs.nixvim.plugins.lualine = {
