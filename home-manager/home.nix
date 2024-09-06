5b8eb210d26cf3affbed94b56fc769cd07843169
krystian <krskibin@gmail.com>
2024-09-06 22:04:37 +0200

    Add cursor config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,9 +9,18 @@
     username = "krystian";
     homeDirectory = "/home/krystian";
     stateVersion = "24.05";
+
     sessionVariables = {
       TERMINAL = "${pkgs.wezterm}/bin/wezterm";
     };
+
+    pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.catppuccin-cursors.mochaDark;
+      name = "Catppuccin-Mocha-Dark-Cursors";
+      size = 16;
+    };
+    
   };
  
 }
