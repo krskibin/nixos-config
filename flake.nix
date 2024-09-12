13b37d38fee62c04e3b87689a3b35f9bc9b937ce
krskibin <krskibin@gmail.com>
2024-09-12 17:16:30 +0200

    Add NixOS unstable channel to flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "NixOS system configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
@@ -14,12 +15,24 @@
     };
   };
 
-  outputs = {self, nixpkgs, home-manager, ...}@inputs :
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    ...
+  }@inputs :
     let 
       system = "x86_64-linux";
     in{
     nixosConfigurations.BD-1 = nixpkgs.lib.nixosSystem {
       inherit system;
+      specialArgs = {
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
       modules = [ 
         ./nixos/configuration.nix
         inputs.nixvim.nixosModules.nixvim
@@ -28,6 +41,9 @@
 
     homeConfigurations.krystian = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.${system};
+      extraSpecialArgs = {
+        pkgs-unstable = import nixpkgs-unstable { inherit system; };
+      };
       modules = [./home-manager/home.nix];
     };
     
