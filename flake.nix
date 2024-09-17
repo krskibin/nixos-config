1d6edbfae6e5162bd790fb9d4330bf9b36d0bc8b
krskibin <krskibin@gmail.com>
2024-09-17 17:01:53 +0200

    Undo upgrade to unstable vol2

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "NixOS system configuration";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
